897fdd2f6135dff0fd5a7e2cccdab6d0e9aa3d49
ChristineRedullas <niceredullasc@gmail.com>
2024-01-29 23:22:19 -0500

    Got an Open Weather API key

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ fetch("https://api.weather.gov")
     function weather {
         getElementByID("")
     }
+
+    //using api from open weather. 
+
+    //https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API%20key}
+
+    // learn how to make api fetch
